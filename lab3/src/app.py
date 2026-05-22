894adefdc31326f933cd27ab3d56d57a9ad76899
Ahmed Daoud <ahmeddaoud@Ahmeds-MacBook-Air.local>
2026-05-22 15:30:12 -0400

    change in heatmap removed pivoting

--- lab3/src/app.py
+++ lab3/src/app.py
@@ -11,8 +11,10 @@
 '''
 
 import dash
-import dash_html_components as html
-import dash_core_components as dcc
+# import dash_html_components as html
+from dash import html
+# import dash_core_components as dcc
+from dash import dcc
 from dash.dependencies import Input, Output
 
 import pandas as pd
