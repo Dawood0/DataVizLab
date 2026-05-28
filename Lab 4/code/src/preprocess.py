4362c6f0806fb31f1c23927cbaa3a493f2c2577e
AzureAD\GabrielleGallant <ggallant@simwell.io>
2026-05-27 21:46:47 -0400

    preprocessing is completed

--- Lab 4/code/src/preprocess.py
+++ Lab 4/code/src/preprocess.py
@@ -1,7 +1,7 @@
 '''
     Contains some functions to preprocess the data used in the visualisation.
 '''
-
+import pandas as pd
 
 def round_decimals(my_df):
     '''
@@ -13,7 +13,8 @@ def round_decimals(my_df):
             The dataframe with rounded numbers
     '''
     # TODO : Round the dataframe
-    return None
+    my_df[['GDP', 'CO2']] = my_df[['GDP', 'CO2']].round(2)
+    return my_df
 
 
 def get_range(col, df1, df2):
@@ -28,8 +29,9 @@ def get_range(col, df1, df2):
         returns:
             The minimum and maximum values across the two dataframes
     '''
-    # TODO : Get the range from the dataframes
-    return []
+    minimum = min(df1[col].min(), df2[col].min())
+    maximum = max(df1[col].max(), df2[col].max())
+    return [minimum, maximum]
 
 
 def combine_dfs(df1, df2):
@@ -48,7 +50,11 @@ def combine_dfs(df1, df2):
             original dataframe.
     '''
     # TODO : Combine the two dataframes
-    return None
+    df1["Year"]= "2000"
+    df2["Year"]= "2015"
+    
+    merged_df= pd.concat([df1,df2])
+    return merged_df
 
 
 def sort_dy_by_yr_continent(my_df):
@@ -61,4 +67,5 @@ def sort_dy_by_yr_continent(my_df):
             The sorted dataframe.
     '''
     # TODO : Sort the dataframe
-    return None
+    my_df = my_df.sort_values(["Year","Continent"], ascending = [True, True])
+    return my_df
