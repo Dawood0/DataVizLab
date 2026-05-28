9518bc8b728338f844006f0508520fce26c3d6be
AzureAD\GabrielleGallant <ggallant@simwell.io>
2026-05-27 21:11:16 -0400

    added tp4

--- lab3/src/heatmap.py
+++ lab3/src/heatmap.py
@@ -23,19 +23,11 @@ def get_figure(data):
 
     # TODO : Create the heatmap. Make sure to set dragmode=False in
     # the layout. Also don't forget to include the hover template.
-#     data = data.rename(columns={"Arrond_Nom": "Neighborhood","Date_Plantation": "Year","Counts": "Trees"})
-#     print(data)
-    
-        
-    # heatmap_data = data.pivot_table(index="Neighborhood",columns="Year",values="Trees",aggfunc="sum",fill_value=0)
+
     heatmap_data = data
     fig = px.imshow(heatmap_data,x=heatmap_data.columns,y=heatmap_data.index,color_continuous_scale="Viridis",labels=dict(x="Year", y="Neighborhood", color="Trees"),aspect="auto")
-
-    #Make sure to set dragmode=False in
     fig.update_layout(dragmode=False)
-
     fig.update_xaxes(tickmode="array",tickvals=list(heatmap_data.columns),ticktext=list(heatmap_data.columns))
-    #Also don't forget to include the hover template.
     fig.update_traces(hovertemplate=hover_template.get_heatmap_hover_template()
 )
 
